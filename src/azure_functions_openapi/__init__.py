9b2940e1ccaa0b6f5558506529e03bdc51a2411a
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-01-23 19:35:06 +0900

    Expose core public APIs at package root

--- src/azure_functions_openapi/__init__.py
+++ src/azure_functions_openapi/__init__.py
@@ -1,6 +1,14 @@
 # src/azure_functions_openapi/__init__.py
 
-from azure_functions_openapi.openapi import OPENAPI_VERSION_3_0, OPENAPI_VERSION_3_1
+from azure_functions_openapi.decorator import openapi
+from azure_functions_openapi.openapi import (
+    OPENAPI_VERSION_3_0,
+    OPENAPI_VERSION_3_1,
+    generate_openapi_spec,
+    get_openapi_json,
+    get_openapi_yaml,
+)
+from azure_functions_openapi.swagger_ui import render_swagger_ui
 
 __version__ = "0.8.0"
 
@@ -8,4 +16,9 @@ __all__ = [
     "__version__",
     "OPENAPI_VERSION_3_0",
     "OPENAPI_VERSION_3_1",
+    "generate_openapi_spec",
+    "get_openapi_json",
+    "get_openapi_yaml",
+    "openapi",
+    "render_swagger_ui",
 ]
