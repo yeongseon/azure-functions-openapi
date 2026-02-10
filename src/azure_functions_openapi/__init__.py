f41d1be7c82b016e2f4dd932601e9d8d77bf77be
Yeongseon Choe <yeongseon.choe@gmail.com>
2026-02-10 10:01:12 +0900

    build: bump version to 0.10.1

--- src/azure_functions_openapi/__init__.py
+++ src/azure_functions_openapi/__init__.py
@@ -10,7 +10,7 @@ from azure_functions_openapi.openapi import (
 )
 from azure_functions_openapi.swagger_ui import render_swagger_ui
 
-__version__ = "0.10.0"
+__version__ = "0.10.1"
 
 __all__ = [
     "__version__",
